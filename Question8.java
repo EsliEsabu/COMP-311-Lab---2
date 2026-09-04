2ada3041a53459888c3a5eb1bde1a4433e73db7f
Esli Esabu <EE24019733@BIUST.AC.BW>
2026-09-04 23:46:52 +0200

    Add Question8-evenodd

--- Question8.java
+++ Question8.java
@@ -1,3 +1,5 @@
+
+//Esli Quest Esabu 24019733
 import java.io.File;
 import java.io.FileWriter;
 import java.util.Scanner;
@@ -5,19 +7,21 @@ import java.util.Scanner;
 
 public class Question8 {
     public static void main(String[]args){
-        try {
+        try {// Open numbers.txt file
             Scanner file = new Scanner (new File ("numbers.txt"));
             
             int evenCount = 0;
             int oddCount = 0;
 
+            // Read every number
             while (file.hasNextInt()){
                 int num = file.nextInt();
-                if (num % 2 == 0) evenCount++;
+                if (num % 2 == 0) evenCount++;// Checking if the number is even or odd
                 else oddCount++;
             }
             file.close();
 
+            // Saving the results to evenodd.txt
             FileWriter writer = new FileWriter("evenodd.txt");
             writer.write("Even numbers: " + evenCount + "\n");
             writer.write("Odd numbers: " + oddCount + "\n");

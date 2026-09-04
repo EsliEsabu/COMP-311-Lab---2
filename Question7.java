7fdbfa45e9d3b765d49b9b4e3294c24388741901
Esli Esabu <EE24019733@BIUST.AC.BW>
2026-09-04 23:49:12 +0200

    Add Question7- statistics

--- Question7.java
+++ Question7.java
@@ -1,18 +1,19 @@
+//Esli Quest Esabu 24019733
 import java.io.File;
 import java.io.FileWriter;
 import java.util.Scanner;
 
 public class Question7 {
     public static void main(String[]args){
-        try {
+        try {// Open numbers.txt
             Scanner file = new Scanner (new File("numbers.txt"));
 int smallest = Integer.MAX_VALUE;
 int largest = Integer.MIN_VALUE;
 long sum = 0;
 int count = 0;
-
+// Read all numbers from the file
 while (file.hasNextInt()){
-    int num = file.nextInt();
+    int num = file.nextInt();// Finding for the smallest and largest number
     if (num < smallest) smallest = num;
     if (num > largest) largest = num;
     sum += num;
@@ -20,9 +21,10 @@ while (file.hasNextInt()){
 }
 
 file.close();
-
+// Calculate the average
 double average = (double) sum / count;
 
+// Saving the results to stats.txt
 FileWriter writer = new FileWriter("stats.txt");
 writer.write("Smallest:"+ smallest + "\n");
 writer.write("Largest:"+ largest + "\n");

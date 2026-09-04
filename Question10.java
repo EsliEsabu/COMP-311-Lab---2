5cf06fe705c306a3a0c611b1240fca4abfb14aaf
Esli Esabu <EE24019733@BIUST.AC.BW>
2026-09-04 23:41:57 +0200

    Add Question10 - searcher

--- Question10.java
+++ Question10.java
@@ -4,15 +4,16 @@ import java.util.Scanner;
 
 public class Question10 {
     public static void main(String[]args) {
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);// Get the number  user
         System.out.println("Enter a number to search for:");
         int target = input.nextInt();
 
         boolean found = false;
         
-        try {
+        try {// Open the file
             Scanner file = new Scanner(new File("students.txt"));
             
+            // Search through the file
             while (file.hasNextLine()){
                 if (file.nextInt() == target) {
                     found = true;
